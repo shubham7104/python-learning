6164cb33841796c528926fcc2a421902d9adec2d
Shubham7104 <sksingh71004@gmail.com>
2025-06-28 05:54:11 +0530

    Adding chapter 7 practice set

---
+++ chapter7PS/01_problem.py
@@ -0,0 +1,5 @@
+# Write a program to print multiplication table of a given number using for loop.
+
+# a = int(input("Enter the number: "))
+for i in range(1, 11):
+    print("5 X i =, 5*i")
---
+++ chapter7PS/02_problem.py
@@ -0,0 +1,9 @@
+'''
+Write a program to greet all the person names stored in a list ‘l’ and which starts
+with S.
+l = ["Harry", "Soham", "Sachin", "Rahul"]
+'''
+
+l = ["Harry", "Soham", "Sachin", "Rahul"]
+a = l.startswith"S"
+print(a)
