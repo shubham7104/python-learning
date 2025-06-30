80491d9f88479b234d0eee4a777099a7aaeaa493
Shubham7104 <sksingh71004@gmail.com>
2025-06-30 17:56:47 +0530

    compressig the line of code

--- project 1/main_shorten.py
+++ project 1/main_shorten.py
@@ -18,20 +18,25 @@ print(f"You chose {reverseDict[you]}\nComputer chose {reverseDict[computer]}")
 if(computer == you):
     print("It's a draw")
 else:
-    if(computer == -1 and you == 1):
+    '''
+    if(computer == -1 and you == 1):(computer - you) = -2
         print("You win")
-    elif(computer == -1 and you == 0):
+    elif(computer == -1 and you == 0):(computer - you) = -1
          print("You Lose!")
-    elif(computer == 1 and you == -1):
+    elif(computer == 1 and you == -1):(computer - you) = 2
          print("You Lose!")
-    elif(computer == 1 and you == 0):
+    elif(computer == 1 and you == 0):(computer - you) = 1
          print("You win!")
-    elif(computer == 0 and you == -1):
+    elif(computer == 0 and you == -1):(computer - you) = 1
          print("You win!")
-    elif(computer == 0 and you == 1):
+    elif(computer == 0 and you == 1):(computer - you) = -1
          print("You Lose!")
     else:
          print("Something went wrong")
-
-
-    
+         
+         The below logic is written on the basis of the value of computer - you
+    '''
+    if((computer - you)) == -1 or ((computer - you) == 2):
+        print("You lose!")
+    else:
+        print("you win!")
