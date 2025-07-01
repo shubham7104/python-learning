d5624a432d4bfee23f20053c4543bc1139158eff
Shubham7104 <sksingh71004@gmail.com>
2025-07-01 21:08:33 +0530

    Updating the chapter 9

---
+++ chapter 9/01_file.py
@@ -0,0 +1,11 @@
+'''
+a = "a very long string with emails"
+
+
+emails = []
+3 seconds
+'''
+f = open("file.txt") # open() is a function helps to open the file
+data = f.read() # .read() helps to read the content of the file
+print(data)
+f.close # it is command to computer for closing the file. ( it is not necessary but  we've to do it)
